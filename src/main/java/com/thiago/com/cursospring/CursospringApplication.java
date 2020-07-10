7bbf484630846445d7c647fbb2b58b494ead319f
Thiago Oliveira <thiagof336@gmail.com>
2020-07-10 19:03:29 -0300

    Criando operacao de instanciacao

--- src/main/java/com/thiago/com/cursospring/CursospringApplication.java
+++ src/main/java/com/thiago/com/cursospring/CursospringApplication.java
@@ -1,13 +1,33 @@
 package com.thiago.com.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.thiago.com.cursospring.domain.Categoria;
+import com.thiago.com.cursospring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
