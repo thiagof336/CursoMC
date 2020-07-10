b6620fc9ba9bcdadb1c2ffb2aa04f6213eccea3c
Thiago Oliveira <thiagof336@gmail.com>
2020-07-10 16:07:54 -0300

    testando a primeira classe

--- src/main/java/com/thiago/com/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/thiago/com/cursospring/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.thiago.com.cursospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thiago.com.cursospring.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informática");
+		
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
 	}
 
 }
